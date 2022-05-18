8a99dd485aadecf41d13e82e84b0276c5ba354f1
James.Maher <james.maher@afacademy.af.edu>
2022-05-18 13:59:17 -0600

    Created a function to build a board of all zeros.

--- Minesweeper/Minesweeper.py
+++ Minesweeper/Minesweeper.py
@@ -1,2 +1,29 @@
+import sys
+from unicodedata import bidirectional
 
-print ("Hello Git")
+class Board:
+
+    board_width = 9
+    board_height = 9
+
+
+    def __init__ (self):
+        self.board_array = self.createBoard(self.board_width, self.board_height)
+        self.printBoardText(self.board_height, self.board_height)
+    # Function to create the back-end array that will hold whether there is a bomb or not at each square.
+    # Function will evolve to contain the number of bombs in surrounding squares and a graphical representation
+    
+    def createBoard (self, width, height):
+        #Initialize the board to zeros. -1 will represent a bomb, number will represent the number of bombs in adjactent spaces.
+        board_array = []
+        for y in range(height):
+           board_array.append([0] * width)
+        return board_array
+
+    def printBoardText (self, width, height):
+        for x in range(width):
+            for y in range (height): 
+                print(self.board_array[x][y], end=" ")
+            print()
+
+game_board = Board()
