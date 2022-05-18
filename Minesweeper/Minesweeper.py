6ef4a809cc51f896524cfb3dc39a416c2ceceb5d
James.Maher <james.maher@afacademy.af.edu>
2022-05-18 14:17:24 -0600

    Added random bomb generation.

--- Minesweeper/Minesweeper.py
+++ Minesweeper/Minesweeper.py
@@ -1,14 +1,16 @@
 import sys
-from unicodedata import bidirectional
+import random
 
 class Board:
 
     board_width = 9
     board_height = 9
-
+    numBombs = 10
 
     def __init__ (self):
         self.board_array = self.createBoard(self.board_width, self.board_height)
+        self.printBoardText(self.board_height, self.board_height)
+        self.placeBombs()
         self.printBoardText(self.board_height, self.board_height)
     # Function to create the back-end array that will hold whether there is a bomb or not at each square.
     # Function will evolve to contain the number of bombs in surrounding squares and a graphical representation
@@ -25,5 +27,11 @@ class Board:
             for y in range (height): 
                 print(self.board_array[x][y], end=" ")
             print()
+
+    def placeBombs (self):
+        for bomb in range(self.numBombs):
+            randX = random.randint(0,(self.board_width-1))
+            randY = random.randint(0,(self.board_height-1))
+            self.board_array[randX][randY] = -1
 
 game_board = Board()
