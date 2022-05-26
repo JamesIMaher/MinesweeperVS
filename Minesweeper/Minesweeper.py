0790c79cafe204150ba961193311b132a6c3fc43
james <james@DESKTOP-N509RAS>
2022-05-26 06:28:03 -0600

    Trying to create a Cell class where each board is made up of a series of cells.

--- Minesweeper/Minesweeper.py
+++ Minesweeper/Minesweeper.py
@@ -4,33 +4,66 @@ import random
 from telnetlib import SE
 import pygame
 
+class GameCell:
+
+    bombPresent = False
+    xPos = 0
+    yPos = 0
+    #Number of mines in surrounding cells
+    numSurroundingMines = 0
+    #Does this show to the user?
+    visible = False
+    probabilitiesMine = []
+
+    def __init__ (self, xPos, yPos):
+        self.xPos = xPos
+        self.yPos = yPos
+
+    def setMine (self):
+        self.bombPresent = True
+
+    def countSurroundingMines (self, numMines):
+        self.numSurroundingMines = numMines
+
+    def userClicked (self):
+        self.visible = True
+
 class Board:
 
-
-    def __init__ (self, width, height, numMines):
+    def __init__ (self, width, height, numMines, visible):
         self.board_width = width
         self.board_height = height
         self.numMines = numMines
         self.board_array = self.createBoard(self.board_width, self.board_height)
-        #self.printBoardText(self.board_height, self.board_height) #print the board with all zeros
-        self.placeBombs()
-        self.determineCounts(self.board_width, self.board_height)
-        self.printBoardText(self.board_height, self.board_height)
+        self.printBoardText(self.board_height, self.board_height) #print the board with all zeros
+        #If the board is not visible, the program is creating the solution board. Otherwise, we are creating the user board.
+        if not visible:
+            self.placeBombs()
+            self.determineCounts(self.board_width, self.board_height)
+            self.printBoardText(self.board_height, self.board_height)
     # Function to create the back-end array that will hold whether there is a bomb or not at each square.
     # Function will evolve to contain the number of bombs in surrounding squares and a graphical representation
     
     def createBoard (self, width, height):
         #Initialize the board to zeros. -1 will represent a bomb, number will represent the number of bombs in adjactent spaces.
         board_array = []
+        x = 0
         for y in range(height):
-           board_array.append([0] * width)
+            for x in range(width):
+                new_cell = GameCell(x,y)
+                self.board_array.append(new_cell)
         return board_array
 
     def printBoardText (self, width, height):
-        for x in range(height):
-            #for y in range (height): 
-            print(*self.board_array[x], sep="\t", end="\n")
-            #print()
+        for y in range(height):
+            for x in range (width):
+                if self.board_array[y][x].bombpresent == True:
+                    print("-1\t")
+                else:
+                    print(self.board_array[y][x].numSurroundingMines + "\t")
+            print("\n")
+                #print(*self.board_array[y][x], sep="\t", end="\n")
+
 
     def placeBombs (self):
         for bomb in range(self.numMines):
@@ -79,19 +112,23 @@ class Board:
     def returnBoardNumber (self, yPos, xPos):
         return self.board_array[yPos][xPos]
 
+
 class UserInteraction:
 
     def __init__ (self):
         userInput = input("Welcome to Minesweeper. Would you like a small, medium, or large board?\n")
         if userInput == "small":
-            self.game_board = Board(9,9,10)
+            self.game_board = Board(9, 9, 10,False)
+            self.visible_board = Board(9, 9, 10, True)
         elif userInput == "medium":
-            self.game_board = Board(16,16,40)
+            self.game_board = Board(16, 16, 40,False)
+            self.visible_board = Board(16, 16, 40,True)
         elif userInput == "large":
-            self.game_board = Board(30,16,99)
+            self.game_board = Board(30, 16, 99, False)
+            self.visible_board = Board(30, 16, 99, True)
         else:
             print("error")
-
+        
 
 class Screen:
 
